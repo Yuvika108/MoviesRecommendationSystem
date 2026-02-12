a6de65699451a87a6464117d54f588fe1d34804f
Srishti Mishra <mishrasrishti108@gmail.com>
2026-02-12 12:40:39 +0530

    Add files via upload

---
+++ MovieRecommendationSystem.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+movies = pd.read_csv("tmdb_5000_movies.csv")
+credits = pd.read_csv("tmdb_5000_credits.csv")
+
+print(movies.head())
+print(credits.head())
